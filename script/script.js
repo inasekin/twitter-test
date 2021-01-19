d88dc4fa8c92b5bdfb5d1d83f266848032aaf6c2
Ivan <nasekinid8591@yandex.ru>
2021-01-19 22:47:31 +0300

    add main classes

--- script/script.js
+++ script/script.js
@@ -1,1 +1,44 @@
 'use strict';
+
+class Posts {
+    constructor(param) {
+        super();
+        this.tweets = param.post;
+    }
+
+    addPost(tweet) {
+        const post = new Post(tweet);
+        this.tweets.push(post);
+    }
+
+    deletePost(id) {
+
+    }
+
+    likePost(id) {
+        
+    }
+}
+
+class Post {
+    constructor(param) {
+        super();
+        this.id = param.id;
+        this.userName = param.userName;
+        this.nickName = param.nickName;
+        this.postData = param.postData;
+        this.label = param.label;
+        this.image = param.image;
+        this.likes = param.likes;
+        this.liked = false;
+    }
+
+    changeLike() {
+        this.liked = !this.liked;
+        if (this.liked) {
+            this.liked++;
+        } else {
+            this.liked--;
+        }
+    }
+}
